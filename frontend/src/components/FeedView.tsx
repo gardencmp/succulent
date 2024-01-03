1d994a49768880e6ac539b782ca94fdbccdc9d34
Anselm <anselm.eickhoff@gmail.com>
2024-01-03 20:49:50 +0000

    Drag and drop skeleton

--- frontend/src/components/FeedView.tsx
+++ frontend/src/components/FeedView.tsx
@@ -1,5 +1,6 @@
 import {
   Brand,
+  ISODate,
   InstagramNotScheduled,
   InstagramPosted,
   InstagramScheduleDesired,
@@ -22,6 +23,14 @@ import {
   MainContent,
   ResponsiveDrawer,
 } from './ResponsiveDrawer';
+import {
+  DndContext,
+  useDroppable,
+  useDraggable,
+  DragOverlay,
+} from '@dnd-kit/core';
+import { snapCenterToCursor } from '@dnd-kit/modifiers';
+import { cn } from '@/lib/utils';
 
 export function FeedView() {
   const draftStates = ['notScheduled'];
@@ -62,89 +71,233 @@ export function FeedView() {
       draftStates.includes(post?.instagram.state!)
   );
 
+  const [draggedPostId, setDraggedPostId] = useState<CoID<Post>>();
+  const draggedPost = brand?.posts?.find((post) => post?.id === draggedPostId);
+
+  const firstPostState = chronologicalScheduledAndPostedPosts[0]?.instagram;
+  const firstPostDate =
+    firstPostState &&
+    (firstPostState.state === 'posted'
+      ? firstPostState.postedAt
+      : firstPostState.scheduledAt);
+
+  const [schedulePreview, setSchedulePreview] = useState<{
+    before?: ISODate;
+    after?: ISODate;
+  }>();
+
   return (
-    <ResponsiveDrawer
-      className="h-[calc(100dvh-10rem)]"
-      initialDrawerHeightPercent={30}
-      minDrawerHeightPercent={10}
+    <DndContext
+      onDragStart={(event) => {
+        setDraggedPostId(event.active.id as CoID<Post>);
+      }}
+      onDragOver={(event) => {
+        setSchedulePreview(event.over?.data?.current);
+      }}
+      onDragEnd={(event) => {
+        setDraggedPostId(undefined);
+        console.log(
+          'Schedule',
+          event.active.id,
+          event.over?.data?.current?.before,
+          event.over?.data?.current?.after
+        );
+      }}
+      modifiers={[snapCenterToCursor]}
     >
-      <MainContent>
-        <Dialog>
-          <div className="grid grid-cols-3 gap-px">
-            <Button
-              variant="outline"
-              className="aspect-square m-0 p-0 h-auto w-auto col-span-1 rounded-none"
-              onClick={() => {
-                if (!brand) return;
-                const draftPost = brand.meta.group.createMap<Post>({
-                  instagram: {
-                    state: 'notScheduled',
-                  },
-                  images: brand.meta.group.createList<ListOfImages>().id,
-                  inBrand: brand.id,
-                });
-                brand.posts?.append(draftPost.id);
-              }}
-            >
-              +
-            </Button>
-            {chronologicalScheduledAndPostedPosts.map(
-              (post) =>
-                post &&
-                post.images?.[0] && (
-                  <div className="col-span-1 aspect-square">
-                    <DialogTrigger asChild>
+      <ResponsiveDrawer
+        className="h-[calc(100dvh-10rem)]"
+        initialDrawerHeightPercent={30}
+        minDrawerHeightPercent={10}
+      >
+        <MainContent>
+          <Dialog>
+            <div className="grid grid-cols-3">
+              <div className="aspect-square m-0 p-1 relative">
+                <Button
+                  variant="outline"
+                  className="aspect-square m-0 p-0 h-full w-full col-span-1 rounded-none"
+                  onClick={() => {
+                    if (!brand) return;
+                    const draftPost = brand.meta.group.createMap<Post>({
+                      instagram: {
+                        state: 'notScheduled',
+                      },
+                      images: brand.meta.group.createList<ListOfImages>().id,
+                      inBrand: brand.id,
+                    });
+                    brand.posts?.append(draftPost.id);
+                  }}
+                >
+                  +
+                </Button>
+                {draggedPostId && <DropGap after={firstPostDate} />}
+              </div>
+
+              {chronologicalScheduledAndPostedPosts.map((post, i) => {
+                const olderPost = chronologicalScheduledAndPostedPosts[i + 1];
+                const olderPostDate =
+                  olderPost &&
+                  (olderPost.instagram.state === 'posted'
+                    ? olderPost.instagram.postedAt
+                    : olderPost.instagram.scheduledAt);
+                return (
+                  post && (
+                    <div className="col-span-1 aspect-square relative p-1">
+                      {/* <DialogTrigger asChild> */}
                       <Button
                         variant="ghost"
                         className="flex m-0 p-0 h-full w-full rounded-none"
-                        onClick={() => setActivePostID(post.id)}
+                        onClick={() => setActivePostID?.(post.id)}
                       >
-                        <img
-                          key={post?.images[0].id}
-                          className="block w-full h-full object-cover shrink-0 opacity-50 outline-none hover:opacity-100"
-                          src={
-                            post.images[0].imageFile?.as(BrowserImage(500))
-                              ?.highestResSrcOrPlaceholder
-                          }
-                        />
+                        <PostImage post={post} />
                       </Button>
-                    </DialogTrigger>
-                  </div>
-                )
-            )}
-          </div>
-          <DialogContent>
-            {activePost?.instagram.state === 'posted' && (
-              <PostComponent post={activePost!} border={false} />
-            )}
-            {activePost?.instagram.state !== 'posted' && (
-              <DraftPostComponent post={activePost!} border={false} />
-            )}
-          </DialogContent>
-        </Dialog>
-      </MainContent>
-      <DrawerOrSidebar>
-        <Button
-          variant="outline"
-          className="mb-6 justify-center"
-          onClick={() => {
-            if (!brand) return;
-            const draftPost = brand.meta.group.createMap<Post>({
-              instagram: {
-                state: 'notScheduled',
-              },
-              images: brand.meta.group.createList<ListOfImages>().id,
-              inBrand: brand.id,
-            });
-            brand.posts?.append(draftPost.id);
-          }}
-        >
-          New Draft
-        </Button>
-        {draftPosts?.map(
-          (post) => post && <DraftPostComponent post={post} styling="mb-3" />
+                      {/* </DialogTrigger> */}
+                      {post.instagram.state !== 'posted' && draggedPostId && (
+                        <DropGap
+                          before={post.instagram.scheduledAt}
+                          after={olderPostDate}
+                        />
+                      )}
+                    </div>
+                  )
+                );
+              })}
+            </div>
+            <DialogContent>
+              {activePost?.instagram.state === 'posted' && (
+                <PostComponent post={activePost!} border={false} />
+              )}
+              {activePost?.instagram.state !== 'posted' && (
+                <DraftPostComponent post={activePost!} border={false} />
+              )}
+            </DialogContent>
+          </Dialog>
+        </MainContent>
+        <DrawerOrSidebar>
+          <Button
+            variant="outline"
+            className="mb-6 justify-center"
+            onClick={() => {
+              if (!brand) return;
+              const draftPost = brand.meta.group.createMap<Post>({
+                instagram: {
+                  state: 'notScheduled',
+                },
+                images: brand.meta.group.createList<ListOfImages>().id,
+                inBrand: brand.id,
+              });
+              brand.posts?.append(draftPost.id);
+            }}
+          >
+            New Draft
+          </Button>
+          {draftPosts?.map(
+            (post) =>
+              post && (
+                <Draggable postId={post.id}>
+                  <DraftPostComponent post={post} styling="mb-3" />
+                </Draggable>
+              )
+          )}
+        </DrawerOrSidebar>
+        <DragOverlay>
+          {draggedPost && (
+            <div
+              className={cn('w-32 h-32 transition-transform duration-75', {
+                'scale-75': !!schedulePreview,
+              })}
+            >
+              <div className="w-32 h-32 shadow-2xl opacity-70">
+                <PostImage post={draggedPost} />
+              </div>
+              {schedulePreview && (
+                <div className="bg-background text-xs p-4 rounded mt-2 -mx-10">
+                  Schedule:
+                  <br />
+                  {schedulePreview.before && (
+                    <p>
+                      Before:{' '}
+                      {new Date(schedulePreview.before).toLocaleString()}
+                    </p>
+                  )}
+                  {schedulePreview.after && (
+                    <p>
+                      After: {new Date(schedulePreview.after).toLocaleString()}
+                    </p>
+                  )}
+                </div>
+              )}
+            </div>
+          )}
+        </DragOverlay>
+      </ResponsiveDrawer>
+    </DndContext>
+  );
+}
+
+function PostImage({ post }: { post: Resolved<Post> }) {
+  return (
+    post.images?.[0] && (
+      <img
+        key={post.images[0].id}
+        className={cn(
+          'block w-full h-full object-cover shrink-0 outline-none hover:opacity-100',
+          {
+            'opacity-50': post.instagram.state !== 'posted',
+          }
         )}
-      </DrawerOrSidebar>
-    </ResponsiveDrawer>
+        src={
+          post.images[0].imageFile?.as(BrowserImage(500))
+            ?.highestResSrcOrPlaceholder
+        }
+      />
+    )
+  );
+}
+
+function DropGap({ before, after }: { before?: ISODate; after?: ISODate }) {
+  const { isOver, setNodeRef } = useDroppable({
+    id: 'dropGap-' + after + '-' + before,
+    data: { after, before },
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      className="absolute top-0 bottom-0 w-20 -right-10 z-10 flex justify-center items-center"
+    >
+      <div
+        className={cn('h-[80%] rounded', {
+          'w-1': isOver,
+          'w-px': !isOver,
+          'bg-neutral-500': !isOver,
+          'bg-pink-500': isOver,
+        })}
+      />
+    </div>
+  );
+}
+
+function Draggable({
+  children,
+  postId,
+}: {
+  children: React.ReactNode;
+  postId: CoID<Post>;
+}) {
+  const { attributes, listeners, setNodeRef, isDragging } = useDraggable({
+    id: postId,
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      className={isDragging ? 'opacity-30' : ''}
+    >
+      {children}
+    </div>
   );
 }
