eb4833f39d4b37f1be702ecbcbd8566f9bbebe2e
Samantha Kellow <sammii.h@icloud.com>
2024-04-12 21:52:20 +0100

    making DraftPostImage reuseable

--- frontend/src/components/draftPost/DraftPostImage.tsx
+++ frontend/src/components/draftPost/DraftPostImage.tsx
@@ -6,27 +6,36 @@ import { Resolved } from 'jazz-react';
 export function DraftPostImage({
   image,
   onDeletePhoto,
+  onClickPhoto,
+  imageSize = 500,
 }: {
   image?: Resolved<Image>;
-  onDeletePhoto: (id: Image['id']) => void;
+  onDeletePhoto?: (id: Image['id']) => void;
+  onClickPhoto?: () => void;
+  imageSize?: number;
 }) {
   if (!image?.imageFile) return;
 
   return (
-    <div className="relative">
-      <Button
-        variant="destructive"
-        className="absolute right-0"
-        // onDeletePhoto={() => setActiveImageId(image.id)}
-        onClick={() => image && onDeletePhoto(image.id)}
-      >
-        x
-      </Button>
+    <div className="relative w-100 h-100">
+      {onDeletePhoto && (
+        <Button
+          variant="destructive"
+          className="absolute right-0"
+          onClick={() => image && onDeletePhoto(image.id)}
+        >
+          x
+        </Button>
+      )}
       <img
         key={image.id}
         className="w-40 h-40 object-cover shrink-0"
-        src={image.imageFile.as(BrowserImage(500))?.highestResSrcOrPlaceholder}
+        src={
+          image.imageFile.as(BrowserImage(imageSize))
+            ?.highestResSrcOrPlaceholder
+        }
         id={image.id}
+        onClick={() => onClickPhoto && onClickPhoto()}
       />
     </div>
   );
