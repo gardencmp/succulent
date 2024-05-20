b03acc2e1e1430eaf5082f06fd67a64d3101da6c
Anselm <anselm.eickhoff@gmail.com>
2024-05-20 14:27:02 +0100

    Sketch out data model for settings

--- frontend/src/dataModel.ts
+++ frontend/src/dataModel.ts
@@ -1,20 +1,48 @@
 import { Account, CoMap, Profile, co } from 'jazz-tools';
 import { ListOfBrands } from './sharedDataModel';
+import { insightTypes } from './pages/settings/PreferencesPage';
 
 export class AccountRoot extends CoMap {
   brands = co.ref(ListOfBrands);
+  settings = co.ref(Settings);
 }
+
+export class Settings extends CoMap {
+  perBrand = co.ref(SettingsPerBrand);
+}
+
+export class PersonalBrandSettings extends CoMap {
+  postInsightsOrder: (typeof insightTypes)[number][] = [];
+}
+
+export class SettingsPerBrand extends CoMap.Record(
+  co.ref(PersonalBrandSettings)
+) {}
 
 export class SucculentAccount extends Account {
   profile = co.ref(Profile);
   root = co.ref(AccountRoot);
 
-  migrate(creationProps?: { name: string }) {
+  async migrate(creationProps?: { name: string }) {
     super.migrate(creationProps);
     if (!this._refs.root) {
       this.root = AccountRoot.create(
         {
           brands: ListOfBrands.create([], { owner: this }),
+        },
+        { owner: this }
+      );
+    }
+    const root = (await this._refs.root!.load())!;
+    if (!root._refs.settings) {
+      root.settings = Settings.create(
+        {
+          perBrand: SettingsPerBrand.create(
+            {
+              // TODO
+            },
+            { owner: this }
+          ),
         },
         { owner: this }
       );
