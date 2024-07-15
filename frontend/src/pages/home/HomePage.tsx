5b960f496c2638250e62b53df167db610cd4a046
Anselm <anselm.eickhoff@gmail.com>
2024-07-15 13:26:11 +0100

    Add reconnect to Meta API button

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -32,81 +32,72 @@ export function HomePage() {
 
   return (
     <main className="p-10 flex flex-col gap-5">
-      {me.root?.metaAPIConnection ? (
-        <>
-          <h1 className="text-2xl">My brands</h1>
-          {/* {JSON.stringify(me.root)} */}
-          <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-2">
-            {me.root?.brands?.map(
-              (brand) =>
-                brand && (
-                  <div className="border rounded p-4">
-                    <BrandView key={brand.id} brand={brand} />
-                  </div>
-                )
-            )}
-            <form
-              onSubmit={async (event) => {
-                event.preventDefault();
-                if (!me.root?.brands) return;
-                const el = event.currentTarget;
-                const brandName = el.brand.value;
+      <h1 className="text-2xl">My brands</h1>
+      {/* {JSON.stringify(me.root)} */}
+      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-2">
+        {me.root?.brands?.map(
+          (brand) =>
+            brand && (
+              <div className="border rounded p-4">
+                <BrandView key={brand.id} brand={brand} />
+              </div>
+            )
+        )}
+        <form
+          onSubmit={async (event) => {
+            event.preventDefault();
+            if (!me.root?.brands) return;
+            const el = event.currentTarget;
+            const brandName = el.brand.value;
 
-                const scheduleWorker = await Account.load(
-                  scheduleWorkerId,
-                  me,
-                  {}
-                );
-                if (!scheduleWorker) {
-                  throw new Error('scheduleWorker unavailable');
-                }
+            const scheduleWorker = await Account.load(scheduleWorkerId, me, {});
+            if (!scheduleWorker) {
+              throw new Error('scheduleWorker unavailable');
+            }
 
-                const brandGroup = Group.create({
-                  owner: me,
-                });
-                brandGroup.addMember(scheduleWorker, 'writer');
+            const brandGroup = Group.create({
+              owner: me,
+            });
+            brandGroup.addMember(scheduleWorker, 'writer');
 
-                const brand = Brand.create(
-                  {
-                    name: brandName,
-                    posts: ListOfPosts.create([], { owner: brandGroup }),
-                    usedLocations: ListOfLocations.create([], {
-                      owner: brandGroup,
-                    }),
-                    usedUserTags: UserTagList.create([], { owner: brandGroup }),
-                  },
-                  { owner: brandGroup }
-                );
+            const brand = Brand.create(
+              {
+                name: brandName,
+                posts: ListOfPosts.create([], { owner: brandGroup }),
+                usedLocations: ListOfLocations.create([], {
+                  owner: brandGroup,
+                }),
+                usedUserTags: UserTagList.create([], { owner: brandGroup }),
+              },
+              { owner: brandGroup }
+            );
 
-                me.root?.brands.push(brand);
-              }}
-              className="border rounded p-4 flex flex-col gap-2"
-            >
-              <Input type="text" name="brand" placeholder="Brand name" />
-              <Button asChild>
-                <input type="submit" value="Add brand" />
-              </Button>
-            </form>
-          </div>
-        </>
-      ) : (
-        <>
-          <h1>Meta API not connected</h1>
-          <Button
-            onClick={() => {
-              window.location =
-                `https://www.facebook.com/v18.0/dialog/oauth?client_id=${'851322152980071'}&redirect_uri=${encodeURIComponent(
-                  (import.meta.env.VITE_SUCCULENT_BACKEND_ADDR ||
-                    'http://localhost:3331') + '/connectFB'
-                )}&state=${me.id}&scope=${encodeURIComponent(
-                  scopes.join(',')
-                )}&response_type=code` as string & Location;
-            }}
-          >
-            Connect to Instagram/Facebook
+            me.root?.brands.push(brand);
+          }}
+          className="border rounded p-4 flex flex-col gap-2"
+        >
+          <Input type="text" name="brand" placeholder="Brand name" />
+          <Button asChild>
+            <input type="submit" value="Add brand" />
           </Button>
-        </>
-      )}
+        </form>
+      </div>
+      <Button
+        onClick={() => {
+          window.location =
+            `https://www.facebook.com/v18.0/dialog/oauth?client_id=${'851322152980071'}&redirect_uri=${encodeURIComponent(
+              (import.meta.env.VITE_SUCCULENT_BACKEND_ADDR ||
+                'http://localhost:3331') + '/connectFB'
+            )}&state=${me.id}&scope=${encodeURIComponent(
+              scopes.join(',')
+            )}&response_type=code` as string & Location;
+        }}
+        className="mr-auto"
+        variant={me.root?.metaAPIConnection ? 'ghost' : 'default'}
+      >
+        {(me.root?.metaAPIConnection ? 'Reconnect' : 'Connect') +
+          ' to Instagram/Facebook'}
+      </Button>
     </main>
   );
 }
