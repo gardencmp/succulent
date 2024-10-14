6b8425df16be50778e12a83ab4818a019ede5587
Anselm <anselm.eickhoff@gmail.com>
2024-10-14 16:56:00 +0100

    Update jazz

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,28 +2,49 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './components/themeProvider.tsx';
-import { createJazzReactContext, PasskeyAuth } from 'jazz-react';
+import {
+  createJazzReactApp,
+  usePasskeyAuth,
+  PasskeyAuthBasicUI,
+} from 'jazz-react';
 import { SucculentAccount } from './dataModel.ts';
 
-export const Jazz = createJazzReactContext<SucculentAccount>({
-  auth: PasskeyAuth<SucculentAccount>({
+export const Jazz = createJazzReactApp({
+  AccountSchema: SucculentAccount,
+});
+export const { useCoState, useAccount, useAcceptInvite } = Jazz;
+
+export function JazzAndAuth({ children }: { children: React.ReactNode }) {
+  const [auth, authState] = usePasskeyAuth({
     appName: 'succulent',
-    accountSchema: SucculentAccount,
     appHostname: window.location.hostname.endsWith('succulent.jazz.tools')
       ? 'succulent.jazz.tools'
       : undefined,
-  }),
-  peer: 'wss://mesh.jazz.tools/?key=succulent-frontend@gcmp.io',
-  storage: 'singleTabOPFS',
-});
-export const { useCoState, useAccount, useAcceptInvite } = Jazz;
+  });
+
+  return (
+    <>
+      <Jazz.Provider
+        auth={auth}
+        storage="singleTabOPFS"
+        // replace `you@example.com` with your email as a temporary API key
+        peer="wss://mesh.jazz.tools/?key=you@example.com"
+      >
+        {children}
+      </Jazz.Provider>
+      {authState.state !== 'signedIn' && (
+        <PasskeyAuthBasicUI state={authState} />
+      )}
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <ThemeProvider>
-    <Jazz.Provider>
+    <JazzAndAuth>
       <App />
-    </Jazz.Provider>
+    </JazzAndAuth>
   </ThemeProvider>
   // </React.StrictMode>
 );
